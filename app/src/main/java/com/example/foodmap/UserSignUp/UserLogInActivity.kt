34287f996f1608e9927cb902eb944daa7216dd80
Anthony Pham <phamofanthony@gmail.com>
2022-12-06 01:04:13 -0600

    Converted other screens to activities

--- app/src/main/java/com/example/foodmap/UserSignUp/UserLogInActivity.kt
+++ app/src/main/java/com/example/foodmap/UserSignUp/UserLogInActivity.kt
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.foodmap.R
 
-class UserSignUpActivity : AppCompatActivity() {
+class UserLogInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user_sign_up)
+        setContentView(R.layout.activity_user_log_in)
     }
 }
